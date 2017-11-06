47e685b00a646cf191fe143055370b7028b91636
Guillaume Dollé <dolle.guillaume@gmail.com>
2017-11-06 22:05:26 +0100

    Add singularity 2.4 bootstrap for feelpp
    Changelogs
    
    - Move files from feelpp/docker repository to feelpp/singularity
    - No need anymore to compute docker size (auto)
    - Use squasfs file system by default to reduce tremendously the size of containers
      (7.7go -> 2.7 go for feelpp-toolboxes image)
    - Read only file system by default (squashfs) except for bind mount!).
    - Include help directly
    	singularity help <imagename>
    - Add test section
    
    Bug/Known issues
    
    - singularity does not use feelpp.env.sh, feelpp.conf.sh and start.sh script that are
      incompatible currently (no sudo!). Summerize environment variable directly into the
      bootstrap script.
    - Test fails with RETVAL 255 write privilege. (--notest for now)
    - Feature would be to use the new SCF standard, to add a list of applications available
      from outside of the container. For example:
      singularity run --app feelpp_qs_laplacian <imagename>
      singularity run --app paraview <imagename>

--- README.md
+++
@@ -1,2 +0,0 @@
-# singularity
-Singularity containers scripts
---
+++ generate_image.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# This script generate singularity bootstrap per docker images
+# Usage: ./generate_bootstrap.sh <image:branch>
+#
+
+source include_path.sh
+
+if [ ! -f "${BOOTSTRAPDIR}/${BOOTSTRAPFILE}" ]; then
+    echo "Bootstrap: ${BOOTSTRAPDIR}/${BOOTSTRAPFILE} not found!"
+    echo "You should run './generate_bootstrap.sh ${BASEIMAGETAG}' first!"
+    exit 1
+fi
+
+# MUST BE SUDO HERE!
+sudo -E ${SINGULARITY_BIN} -vvv build --force --notest "${IMAGEDIR}/${SINGULARITYIMAGE}" "${BOOTSTRAPDIR}/${BOOTSTRAPFILE}"
