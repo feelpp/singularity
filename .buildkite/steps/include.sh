3b2f327a0d54e2388d91bb6e9c1ef6981d60d990
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2018-06-13 09:57:04 +0200

    up

--- .buildkite/steps/include.sh
+++ .buildkite/steps/include.sh
@@ -6,13 +6,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 CONTAINERS_YAML=${SCRIPT_DIR}/../containers.yml
 
 # List public/private folder girder id.
-PRIVATE_CONTAINERS_GIRDER_ID_LIST=`cat "${CONTAINERS_YAML}" | yq -r ".private[].girder_folder_id"`
+# comment this avoid building private images
+PRIVATE_CONTAINERS_GIRDER_ID_LIST= #`cat "${CONTAINERS_YAML}" | yq -r ".private[].girder_folder_id"`
 PUBLIC_CONTAINERS_GIRDER_ID_LIST=`cat "${CONTAINERS_YAML}" | yq -r ".public[].girder_folder_id"`
 
 # Variable that set the EXPR variable used in CONTAINER_LIST.
-PRIVATE_CONTAINERS_PARSE_EXPR='EXPR=".private[] | select(.girder_folder_id==\"$id\") | .containers[]"'
+# comment this avoid building private images
+PRIVATE_CONTAINERS_PARSE_EXPR= # 'EXPR=".private[] | select(.girder_folder_id==\"$id\") | .containers[]"'
 PUBLIC_CONTAINERS_PARSE_EXPR='EXPR=".public[] | select(.girder_folder_id==\"$id\") | .containers[]"'
 
 # Variable that set the CLIST variable.
 CONTAINERS_LIST='CLIST=`cat "${CONTAINERS_YAML}" | yq -r "${EXPR}"`'
-
